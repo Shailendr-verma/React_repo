884fe3f84d7d7cc6f05898497fe4b8f99eacba18
Shailendra verma <shailendra1630verma@gmail.com>
2025-07-30 00:26:26 +0530

    done

--- 06currencyCovert/src/components/InputsBox.jsx
+++ 06currencyCovert/src/components/InputsBox.jsx
@@ -29,7 +29,8 @@ function InputsBox({
                     onChange={(e) => onAmountChange(Number(e.target.value))}
                 />
             </div>
-            <div className='w-1/2 flex flex-wrap justify-end text-right  '>
+            <div className='w-1/2 flex flex-wrap justify-end text-right 
+       '>
                 <p className='text-black/40 mb-2 w-full'>Currency Type</p>
 
                 <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
